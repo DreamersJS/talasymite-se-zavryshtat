5e1b447dc046562d42be33dcadca0796fabb72f4
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-06-09 15:07:42 +0300

    add: episodes 1-12

---
+++ n/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
