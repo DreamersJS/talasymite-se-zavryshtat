82513132031e22a3c54505cd1c5e740367aa64b8
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-06-24 14:43:51 +0300

    add: adventureDiary.js

---
+++ textAdventure/src/adventureDiary.js
@@ -0,0 +1,21 @@
+const adventureDiary = {
+  bag: [],
+  bagCarrier: "",
+  healthCondition: {
+    healthy: true,
+    sick: false,
+    injuredWithFork: false,
+  },
+  mushroomProperties: {
+    yellow: 'увеличава онзи, който хапне от нея.', show: false,
+    blue: 'смалява многократно консуматора си.', show: false,
+    red: 'силно намалява теглото.', show: false,
+    green: 'увеличава теглото.', show: false,
+    black: 'невероятно отровна, убива жертвата за броени минути.', show: false,
+  },
+  secret: {
+    show: false,
+    text: "Според едно древно предсказание на Дървеняк му е предречено, че ще издъхне моментално, ако го поръси и капка вода.",
+  },
+
+};
