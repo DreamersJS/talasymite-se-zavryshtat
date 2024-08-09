fabb6f2fc6ad990a2881ed636e2e3a22efc83e03
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-08-09 22:29:35 +0300

    works in build mode

--- textAdventure/src/App.jsx
+++ textAdventure/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Game } from './components/Game.jsx';
 // import { Header } from './components/Header.jsx';
 
 function App() {
-
   return (
-    <>
-    {/* <Header></Header> */}
-      <Game></Game>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/game/:page" element={<Game />} />
+        <Route path="/" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
