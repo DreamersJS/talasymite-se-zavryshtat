4c904403617e77cc957045cea2ef67431e262cb6
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2025-06-25 16:32:04 +0300

    add tooltip description on hover

--- textAdventure/src/components/Inventory/Inventory.jsx
+++ textAdventure/src/components/Inventory/Inventory.jsx
@@ -23,33 +23,51 @@ export const Inventory = ({ title, inventory, onTrade, tradeAction, prices }) =>
         </div>
         {Object.keys(inventory.bag).map(item => (
           <li key={item}>
-            {item}
-            <button className='quantity-minus' onClick={() => minusOne(item)} disabled={(quantities[item] || 1) <= 1}></button>
-            (x{quantities[item] || 1})
-            <button className='quantity-plus' onClick={() => plusOne(item)} disabled={(quantities[item] || 1) >= inventory.bag[item].quantity}></button>
+            {/* {item} */}
+            <div  className="tooltip-wrapper">
 
+            
+<strong>
+{itemsData[item].displayName}
+</strong>
 
-            {itemsData[item]?.sellable !== false && (
-              <>
-                Price: {
-                  tradeAction === 'Buy'
-                    ? prices[item]?.buy * (quantities[item] || 1)
-                    : prices[item]?.sell * (quantities[item] || 1)
-                }
-                <button
-                  className="trade-action"
-                  onClick={() => onTrade(item, quantities[item] || 1)}
-                  disabled={itemsData[item]?.sellable === false}
-                >
-                  {tradeAction}
-                </button>
-              </>
-            )}
-            {itemsData[item]?.sellable === false && (
-              <span className="unsellable-note">(Not sellable)</span>
-            )}
+{
+ itemsData[item].description && itemsData[item]?.showDescription !== false && (
+<span className='tooltip-text'>
+{itemsData[item].description}
+</span>
+  )
+}
 
-          </li>
+            {/* sellable */}
+            <>
+              {itemsData[item]?.sellable !== false ? (
+                <>
+                  {/* +- quantity buttons */}
+                  <button className='quantity-minus' onClick={() => minusOne(item)} disabled={(quantities[item] || 1) <= 1}></button>
+                  (x{quantities[item] || 1})
+                  <button className='quantity-plus' onClick={() => plusOne(item)} disabled={(quantities[item] || 1) >= inventory.bag[item].quantity}></button>
+
+                  {/* buy sell actions */}
+                  Price: {
+                    tradeAction === 'Buy'
+                      ? prices[item]?.buy * (quantities[item] || 1)
+                      : prices[item]?.sell * (quantities[item] || 1)
+                  }
+                  <button
+                    className="trade-action"
+                    onClick={() => onTrade(item, quantities[item] || 1)}
+                    disabled={itemsData[item]?.sellable === false}
+                  >
+                    {tradeAction}
+                  </button>
+                </>
+              ) : <span className="unsellable-note">(Not sellable)</span>
+              }
+            </>
+
+            </div>
+            </li>
         ))}
       </ul>
     </div>
